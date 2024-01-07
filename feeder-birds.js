6b3037bd7cb3d425338d4a042a0d557794968566
Peter Babcock <153126754+pjbabcock@users.noreply.github.com>
2024-01-07 17:56:58 -0500

    first try at pulling checkbox data and marking present in jan if checked

--- feeder-birds.js
+++ feeder-birds.js
@@ -2,6 +2,20 @@ function addObservation() {
   //get table
   let table = document.getElementById("observation-table");
 
+  //get checkbox data
+  const month1 = document.getElementById("month1").value;
+  const month2 = document.getElementById9("month2").value;
+  const month3 = document.getElementById("month3").value;
+  const month4 = document.getElementById9("month4").value;
+  const month5 = document.getElementById("month5").value;
+  const month6 = document.getElementById9("month6").value;
+  const month7 = document.getElementById("month7").value;
+  const month8 = document.getElementById9("month8").value;
+  const month9 = document.getElementById("month9").value;
+  const month10 = document.getElementById9("month10").value;
+  const month11 = document.getElementById("month11").value;
+  const month12 = document.getElementById9("month12").value;
+
   //create row
   let row = table.insertRow(-1);
 
@@ -20,10 +34,12 @@ function addObservation() {
   let c12 = row.insertCell(11);
   let c13 = row.insertCell(12);
 
-  //fill cells
+  //fill species name
   c1.innerHTML = document.getElementById("species-name-input").value;
-
-  //test
-  console.log("test")
+  
+  //fill months
+  if (month1 === jan) {
+    c1.classList.add("present")
+  }
 
 }
