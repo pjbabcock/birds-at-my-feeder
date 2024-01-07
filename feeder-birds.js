100f194340cc89ed2cd52550038acd0fc51e6414
Peter <153126754+pjbabcock@users.noreply.github.com>
2024-01-06 19:37:49 -0500

    created js script to add form data to table

---
+++ feeder-birds.js
@@ -0,0 +1,25 @@
+Function addObservation() {
+  //get table
+  let table = document.getElementById("observation-table");
+
+  //create row
+  let row = table.insertRow(-1);
+
+  //create cells
+  let c1 = row.insertCell(0);
+  let c2 = row.insertCell(1);
+  let c3 = row.insertCell(2);
+  let c4 = row.insertCell(3);
+  let c5 = row.insertCell(4);
+  let c6 = row.insertCell(5);
+  let c7 = row.insertCell(6);
+  let c8 = row.insertCell(7);
+  let c9 = row.insertCell(8);
+  let c10 = row.insertCell(9);
+  let c11 = row.insertCell(10);
+  let c12 = row.insertCell(11);
+
+  //fill cells
+  c1.innertext = document.getElementById("species-name-input").value
+  
+}
