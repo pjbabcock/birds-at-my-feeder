525cecce1b1eb27beaf182a57fd96227316efa0f
Peter Babcock <153126754+pjbabcock@users.noreply.github.com>
2024-01-07 22:32:05 -0500

    implemented function to fill month cells from checkbox data

--- feeder-birds.js
+++ feeder-birds.js
@@ -2,44 +2,35 @@ function addObservation() {
   //get table
   let table = document.getElementById("observation-table");
 
-  //get checkbox data
-  const month1 = document.getElementById("month1").value;
-  const month2 = document.getElementById("month2").value;
-  const month3 = document.getElementById("month3").value;
-  const month4 = document.getElementById("month4").value;
-  const month5 = document.getElementById("month5").value;
-  const month6 = document.getElementById("month6").value;
-  const month7 = document.getElementById("month7").value;
-  const month8 = document.getElementById("month8").value;
-  const month9 = document.getElementById("month9").value;
-  const month10 = document.getElementById("month10").value;
-  const month11 = document.getElementById("month11").value;
-  const month12 = document.getElementById("month12").value;
-
   //create row
   let row = table.insertRow(-1);
 
   //create cells
-  let c1 = row.insertCell(0);
-  let c2 = row.insertCell(1);
-  let c3 = row.insertCell(2);
-  let c4 = row.insertCell(3);
-  let c5 = row.insertCell(4);
-  let c6 = row.insertCell(5);
-  let c7 = row.insertCell(6);
-  let c8 = row.insertCell(7);
-  let c9 = row.insertCell(8);
-  let c10 = row.insertCell(9);
-  let c11 = row.insertCell(10);
-  let c12 = row.insertCell(11);
-  let c13 = row.insertCell(12);
+  let c0 = row.insertCell(0);
+  let c1 = row.insertCell(1);
+  let c2 = row.insertCell(2);
+  let c3 = row.insertCell(3);
+  let c4 = row.insertCell(4);
+  let c5 = row.insertCell(5);
+  let c6 = row.insertCell(6);
+  let c7 = row.insertCell(7);
+  let c8 = row.insertCell(8);
+  let c9 = row.insertCell(9);
+  let c10 = row.insertCell(10);
+  let c11 = row.insertCell(11);
+  let c12 = row.insertCell(12);
 
   //fill species name
-  c1.innerHTML = document.getElementById("species-name-input").value;
+  c0.innerHTML = document.getElementById("species-name-input").value;
   
   //fill months
-  if (month1 === "jan") {
-    c2.classList.add("present")
+  let newCells = [c1, c2, c3, c4, c5, c6, c7, c8, c9, c10, c11, c12]
+  
+  for (let i = 0; i < 12; i++) {
+    if (document.getElementById(`month${i+1}`).checked) {
+      newCells[i].classList.add("present")
+    }
+    console.log("test")
   }
 
 }
