789e879b0b4715a8ae1a0c11974c6daeb1d5ebfa
Peter <153126754+pjbabcock@users.noreply.github.com>
2024-01-10 23:33:30 -0500

    implementing autocomplete

--- feeder-birds.js
+++ feeder-birds.js
@@ -124,14 +124,14 @@ function togglePresent(boxId) {
 }
 
 //Autocompleter
-import autocomplete from "autocompleter";
+var autocomplete = require('autocompleter');
 
 var countries = [
   { label: 'United Kingdom', value: 'UK' },
   { label: 'United States', value: 'US' }
 ];
 
-var input = document.getElementById('country');
+var input = document.getElementById("species-name-input");
 
 autocomplete({
   input: input,
