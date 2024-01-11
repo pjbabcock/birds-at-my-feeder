c4cd14139ba0f5cec540034b9cce5c69a0ad6b8e
Peter Babcock <153126754+pjbabcock@users.noreply.github.com>
2024-01-11 13:45:08 -0500

    removed autocompleter package

--- feeder-birds.js
+++ feeder-birds.js
@@ -124,24 +124,3 @@ function togglePresent(boxId) {
 }
 
 //Autocompleter
-var autocomplete = require('autocompleter');
-
-var countries = [
-  { label: 'United Kingdom', value: 'UK' },
-  { label: 'United States', value: 'US' }
-];
-
-var input = document.getElementById("species-name-input");
-
-autocomplete({
-  input: input,
-  fetch: function(text, update) {
-      text = text.toLowerCase();
-      // you can also use AJAX requests instead of preloaded data
-      var suggestions = countries.filter(n => n.label.toLowerCase().startsWith(text))
-      update(suggestions);
-  },
-  onSelect: function(item) {
-      input.value = item.label;
-  }
-});
