76cef18ff650b293f5638ab7f75867b5c56842a7
Peter <153126754+pjbabcock@users.noreply.github.com>
2024-01-15 22:29:50 -0500

    add onclick functionality to month cells in new feeder table

--- feeder-birds.js
+++ feeder-birds.js
@@ -251,6 +251,7 @@ function addFeeder() {
   for (let i=0; i < 12; i++) {
     newSubCells[i].classList.add("month-cell");
     newSubCells[i].id = `month${i+1}_${newestTableNumber}`;
+    newSubCells[i].setAttribute("onclick", `togglePresent(${newSubCells[i].id})`);
   }
 }
 
