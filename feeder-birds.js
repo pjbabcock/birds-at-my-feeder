d70c96084d0ab23425d3e27e09e063b31883b150
Peter Babcock <153126754+pjbabcock@users.noreply.github.com>
2024-01-19 15:46:03 -0500

    added functionality for deleteFeeder

--- feeder-birds.js
+++ feeder-birds.js
@@ -140,9 +140,180 @@ function addFeeder() {
   document.getElementById("feeder-data-container").appendChild(newDiv);
 
   //create feeder title and append to DOM
-  let feederTitle = document.createElement("h2")
-  feederTitle.innerHTML = `Feeder ${newestTableNumber+1}`
-  newDiv.appendChild(feederTitle)
+  let feederTitle = document.createElement("h2");
+  feederTitle.innerHTML = `Feeder ${newestTableNumber+1}`;
+  newDiv.appendChild(feederTitle);
+
+  //create details button and append to DOM
+  let detailsButton = document.createElement("button");
+  detailsButton.id = `details-button_feederTable${newestTableNumber}`;
+  detailsButton.classList.add("details-button");
+  detailsButton.innerHTML = "Details";
+  detailsButton.setAttribute("onclick", `showDetails('feederTable${newestTableNumber}')`);
+  newDiv.appendChild(detailsButton);
+
+  //create hide-details button and append to DOM
+  let hideButton = document.createElement("button");
+  hideButton.id = `hide-details-button_feederTable${newestTableNumber}`;
+  hideButton.classList.add("hide-details-button");
+  hideButton.classList.add("hidden");
+  hideButton.innerHTML = "Hide";
+  hideButton.setAttribute("onclick", `hideDetails('feederTable${newestTableNumber}')`);
+  newDiv.appendChild(hideButton);
+
+  //create metadata container and append to DOM
+  let metaDiv = document.createElement("div");
+  metaDiv.id = `metadata-form-container_feederTable${newestTableNumber}`;
+  metaDiv.classList.add("metadata-form-container");
+  metaDiv.classList.add("hidden");
+  newDiv.appendChild(metaDiv);
+
+  //create metadata form and append to DOM
+  //feeder type
+  let typeLabel = document.createElement("label")
+  typeLabel.htmlFor = `feeder-type_feederTable${newestTableNumber}`;
+  typeLabel.classList.add("feeder-type-label");
+  typeLabel.innerHTML = "Feeder type:";
+  metaDiv.appendChild(typeLabel);
+  let typeSelect = document.createElement("select");
+  typeSelect.id = `feeder-type_feederTable${newestTableNumber}`;
+  typeSelect.classList.add("feeder-type");
+  metaDiv.appendChild(typeSelect);
+  let typeSelectOption1 = document.createElement("option");
+  typeSelectOption1.innerHTML = "Please select an option";
+  typeSelect.appendChild(typeSelectOption1);
+  let typeSelectOption2 = document.createElement("option");
+  typeSelectOption2.innerHTML = "Tube";
+  typeSelect.appendChild(typeSelectOption2);
+  let typeSelectOption3 = document.createElement("option");
+  typeSelectOption3.innerHTML = "Hopper";
+  typeSelect.appendChild(typeSelectOption3);
+  let typeSelectOption4 = document.createElement("option");
+  typeSelectOption4.innerHTML = "Tray";
+  typeSelect.appendChild(typeSelectOption4);
+  let typeSelectOption5 = document.createElement("option");
+  typeSelectOption5.innerHTML = "Window";
+  typeSelect.appendChild(typeSelectOption5);
+  let typeSelectOption6 = document.createElement("option");
+  typeSelectOption6.innerHTML = "Hummingbird";
+  typeSelect.appendChild(typeSelectOption6);
+  let typeSelectOption7 = document.createElement("option");
+  typeSelectOption7.innerHTML = "Nyjer/thistle";
+  typeSelect.appendChild(typeSelectOption7);
+  let typeSelectOption8 = document.createElement("option");
+  typeSelectOption8.innerHTML = "Suet";
+  typeSelect.appendChild(typeSelectOption8);
+  let typeSelectOption9 = document.createElement("option");
+  typeSelectOption9.innerHTML = "Wreath";
+  typeSelect.appendChild(typeSelectOption9);
+  let typeSelectOption10 = document.createElement("option");
+  typeSelectOption10.innerHTML = "Oriole";
+  typeSelect.appendChild(typeSelectOption10);
+  metaDiv.appendChild(document.createElement("br"));
+  //food types
+  let foodLabel = document.createElement("label")
+  foodLabel.htmlFor = `food-types_feederTable${newestTableNumber}`;
+  foodLabel.classList.add("food-types-label");
+  foodLabel.innerHTML = "Food types:";
+  metaDiv.appendChild(foodLabel);
+  let foodSelect = document.createElement("select");
+  foodSelect.id = `food-types_feederTable${newestTableNumber}`;
+  foodSelect.classList.add("food-types");
+  metaDiv.appendChild(foodSelect);
+  let foodSelectOption1 = document.createElement("option");
+  foodSelectOption1.innerHTML = "Hold ctrl to select multiple";
+  foodSelect.appendChild(foodSelectOption1);
+  let foodSelectOption2 = document.createElement("option");
+  foodSelectOption2.innerHTML = "Sunflower";
+  foodSelect.appendChild(foodSelectOption2);
+  let foodSelectOption3 = document.createElement("option");
+  foodSelectOption3.innerHTML = "Safflower";
+  foodSelect.appendChild(foodSelectOption3);
+  let foodSelectOption4 = document.createElement("option");
+  foodSelectOption4.innerHTML = "Nyjer/thistle";
+  foodSelect.appendChild(foodSelectOption4);
+  let foodSelectOption5 = document.createElement("option");
+  foodSelectOption5.innerHTML = "Corn";
+  foodSelect.appendChild(foodSelectOption5);
+  let foodSelectOption6 = document.createElement("option");
+  foodSelectOption6.innerHTML = "Peanuts";
+  foodSelect.appendChild(foodSelectOption6);
+  let foodSelectOption7 = document.createElement("option");
+  foodSelectOption7.innerHTML = "Milo/sorghum";
+  foodSelect.appendChild(foodSelectOption7);
+  let foodSelectOption8 = document.createElement("option");
+  foodSelectOption8.innerHTML = "Millet";
+  foodSelect.appendChild(foodSelectOption8);
+  let foodSelectOption9 = document.createElement("option");
+  foodSelectOption9.innerHTML = "Flax";
+  foodSelect.appendChild(foodSelectOption9);
+  let foodSelectOption10 = document.createElement("option");
+  foodSelectOption10.innerHTML = "Rapeseed";
+  foodSelect.appendChild(foodSelectOption10);
+  let foodSelectOption11 = document.createElement("option");
+  foodSelectOption11.innerHTML = "Canary seed";
+  foodSelect.appendChild(foodSelectOption11);
+  let foodSelectOption12 = document.createElement("option");
+  foodSelectOption12.innerHTML = "Suet";
+  foodSelect.appendChild(foodSelectOption12);
+  let foodSelectOption13 = document.createElement("option");
+  foodSelectOption13.innerHTML = "Nectar";
+  foodSelect.appendChild(foodSelectOption13);
+  let foodSelectOption14 = document.createElement("option");
+  foodSelectOption14.innerHTML = "Dried fruit";
+  foodSelect.appendChild(foodSelectOption14);
+  let foodSelectOption15 = document.createElement("option");
+  foodSelectOption15.innerHTML = "Fresh fruit";
+  foodSelect.appendChild(foodSelectOption15);
+  let foodSelectOption16 = document.createElement("option");
+  foodSelectOption16.innerHTML = "Jelly";
+  foodSelect.appendChild(foodSelectOption16);
+  let foodSelectOption17 = document.createElement("option");
+  foodSelectOption17.innerHTML = "Mealworm";
+  foodSelect.appendChild(foodSelectOption17);
+  metaDiv.appendChild(document.createElement("br"));
+  //feeder location
+  let locationLabel = document.createElement("label")
+  locationLabel.htmlFor = `feeder-location_feederTable${newestTableNumber}`;
+  locationLabel.classList.add("feeder-location-label");
+  locationLabel.innerHTML = "Feeder location:";
+  metaDiv.appendChild(locationLabel);
+  let locationSelect = document.createElement("select");
+  locationSelect.id = `feeder-location_feederTable${newestTableNumber}`;
+  locationSelect.classList.add("feeder-location");
+  metaDiv.appendChild(locationSelect);
+  let locationSelectOption1 = document.createElement("option");
+  locationSelectOption1.innerHTML = "Please select an option";
+  locationSelect.appendChild(locationSelectOption1);
+  let locationSelectOption2 = document.createElement("option");
+  locationSelectOption2.innerHTML = "Backyard";
+  locationSelect.appendChild(locationSelectOption2);
+  let locationSelectOption3 = document.createElement("option");
+  locationSelectOption3.innerHTML = "Front yard";
+  locationSelect.appendChild(locationSelectOption3);
+  metaDiv.appendChild(document.createElement("br"));
+  //feeder distance
+  let distanceLabel = document.createElement("label")
+  distanceLabel.htmlFor = `feeder-distance_feederTable${newestTableNumber}`;
+  distanceLabel.classList.add("feeder-distance-label");
+  distanceLabel.innerHTML = "Distance from house:";
+  metaDiv.appendChild(distanceLabel);
+  let distanceSelect = document.createElement("select");
+  distanceSelect.id = `feeder-distance_feederTable${newestTableNumber}`;
+  distanceSelect.classList.add("feeder-distance");
+  metaDiv.appendChild(distanceSelect);
+  let distanceSelectOption1 = document.createElement("option");
+  distanceSelectOption1.innerHTML = "Please select an option";
+  distanceSelect.appendChild(distanceSelectOption1);
+  let distanceSelectOption2 = document.createElement("option");
+  distanceSelectOption2.innerHTML = "Less than 3 feet";
+  distanceSelect.appendChild(distanceSelectOption2);
+  let distanceSelectOption3 = document.createElement("option");
+  distanceSelectOption3.innerHTML = "Between 3 and 30 feet";
+  distanceSelect.appendChild(distanceSelectOption3);
+  let distanceSelectOption4 = document.createElement("option");
+  distanceSelectOption4.innerHTML = "More than 30 feet";
+  distanceSelect.appendChild(distanceSelectOption4);
 
   //Create feeder table
   let newTable = document.createElement("table");
@@ -267,44 +438,43 @@ function addFeeder() {
     newSubCells[i].setAttribute("onclick", `togglePresent(${newSubCells[i].id})`);
   }
 
-  //create Add button
+  //create Add button and append to DOM
   let newAddButton = document.createElement("button");
   newAddButton.id = `add-button-${newestTableNumber}`;
   newAddButton.type = "button";
   newAddButton.innerHTML = "Add";
   newAddButton.setAttribute("onclick", `addObservation("${newTable.id}")`);
-
-  //append Add button to submission table
   s13.appendChild(newAddButton);
 
-  //create row for all year checkbox
+  //create cells for checkbox and append to DOM
   let allYearRow = newSubTable.insertRow(-1);
-
-  //add cells to all year row
   y0 = allYearRow.insertCell(0);
   y1 = allYearRow.insertCell(1);
-
-  //format cells
   y0.classList.add("blank-cell");
   y1.classList.add("all-year-cell");
 
-  //create all year checkbox
+  //create all year checkbox and append to DOM
   let newCheckbox = document.createElement("input");
   newCheckbox.type = "checkbox";
   newCheckbox.id = `all-year_${newTable.id}`;
   newCheckbox.classList.add("all-year");
   newCheckbox.setAttribute("onclick", `checkAll('${newTable.id}')`);
-
-  //creat all year label
   let newCheckLabel = document.createElement("label");
   newCheckLabel.htmlFor = `all-year_${newTable.id}`;
   newCheckLabel.id = `all-year-label_${newTable.id}`;
   newCheckLabel.classList.add("all-year-label");
   newCheckLabel.innerHTML = "All year";
-
-  //append checkbox and label to cell
   y1.appendChild(newCheckbox);
   y1.appendChild(newCheckLabel);
+
+  //create delete button and append to DOM
+  let deleteButton = document.createElement("button");
+  deleteButton.type = "button";
+  deleteButton.id = `delete-button_${newTable.id}`;
+  deleteButton.classList.add("delete-button");
+  deleteButton.innerHTML = "Delete feeder";
+  deleteButton.setAttribute("onclick", `deleteFeeder('${newTable.id}')`);
+  newDiv.appendChild(deleteButton);
 
   //apply autocomplete to all inputs
   const elements = document.querySelectorAll(".autocomplete");
@@ -366,6 +536,14 @@ function selectFeeder(tableID) {
   //Show selected table
   document.getElementById(`feeder-container_${tableID}`).classList.remove("hidden");
   console.log(`I showed feeder-container_${tableID}`);
+}
+
+//Delete feeder table:
+function deleteFeeder(tableID) {
+  if (confirm("Do you want to delete this feeder?") === true) {
+    document.getElementById(`feeder-container_${tableID}`).remove();
+    document.getElementById(`selector_${tableID}`).remove();
+  }
 }
 
 //Autocomplete array
